7313b64edfc49d6e3773f1691b4bbab50a8f045b
Yanwen Xu <xuyanwen2012@gmail.com>
2025-02-10 11:40:04 -0800

    save

--- pipe/cifar-sparse-vk/main.cpp
+++ pipe/cifar-sparse-vk/main.cpp
@@ -4,36 +4,43 @@
 #include <cstdint>
 #include <memory_resource>
 
+#include "affinity.hpp"
 #include "app.hpp"
 #include "cifar-sparse/omp/sparse_kernel.hpp"
 #include "cifar-sparse/vulkan/vk_dispatcher.hpp"
+#include "spdlog/common.h"
 
-template <int start_stage, int end_stage>
+enum class ProcessorType {
+  kAny,
+  kLittleCore,
+  kMediumCore,
+  kBigCore,
+};
+
+template <int start_stage, int end_stage, ProcessorType processor_type>
 void run_stages(cifar_sparse::AppData* app_data) {
   static_assert(start_stage >= 1 && end_stage <= 9,
                 "Stage range out of bounds");
   static_assert(start_stage <= end_stage, "start_stage must be <= end_stage");
 
-  // Function table to map stage numbers to their corresponding functions
-  constexpr auto stage_functions = [](auto* app_data) {
-    constexpr auto stages = std::array{cifar_sparse::omp::process_stage_1,
-                                       cifar_sparse::omp::process_stage_2,
-                                       cifar_sparse::omp::process_stage_3,
-                                       cifar_sparse::omp::process_stage_4,
-                                       cifar_sparse::omp::process_stage_5,
-                                       cifar_sparse::omp::process_stage_6,
-                                       cifar_sparse::omp::process_stage_7,
-                                       cifar_sparse::omp::process_stage_8,
-                                       cifar_sparse::omp::process_stage_9};
-
-    // Execute only the required number of stages (up to `n_stages`)
-    for (int i = start_stage; i < end_stage; ++i) {
-      stages[i](*app_data);
-    }
-  };
-
 #pragma omp parallel
-  { stage_functions(app_data); }
+  {
+    // Bind to core if needed:
+    if constexpr (processor_type == ProcessorType::kLittleCore) {
+      bind_thread_to_core(g_little_cores);
+    } else if constexpr (processor_type == ProcessorType::kMediumCore) {
+      bind_thread_to_core(g_medium_cores);
+    } else if constexpr (processor_type == ProcessorType::kBigCore) {
+      bind_thread_to_core(g_big_cores);
+    }
+
+    // Generate a compile-time sequence for the range [start_stage, end_stage]
+    []<std::size_t... I>(std::index_sequence<I...>,
+                         cifar_sparse::AppData& data) {
+      // Each I is offset by (start_stage - 1)
+      ((cifar_sparse::omp::run_stage<start_stage + I>(data)), ...);
+    }(std::make_index_sequence<end_stage - start_stage + 1>{}, *app_data);
+  }
 }
 
 // ---------------------------------------------------------------------
@@ -51,23 +58,31 @@ std::atomic<bool> done(false);
                                            std::pmr::memory_resource* mr) {
   std::vector<Task> tasks(num_tasks);
 
+  spdlog::set_level(spdlog::level::off);
+
   for (uint32_t i = 0; i < num_tasks; ++i) {
     tasks[i] = Task{i, new cifar_sparse::AppData(mr)};
 
     // We actually need to run all the stages first.
-    run_stages<1, 9>(tasks[i].app_data);
+    run_stages<1, 9, ProcessorType::kAny>(tasks[i].app_data);
   }
+
+  spdlog::set_level(spdlog::level::debug);
 
   return tasks;
 }
 
 // ---------------------------------------------------------------------
-//
+// Tmp
 // ---------------------------------------------------------------------
 
 void tmp() {
   auto mr = cifar_sparse::vulkan::Singleton::getInstance().get_mr();
   auto tasks = init_tasks(10, mr);
+
+  run_stages<1, 4, ProcessorType::kLittleCore>(tasks[0].app_data);
+  run_stages<5, 6, ProcessorType::kMediumCore>(tasks[0].app_data);
+  run_stages<7, 9, ProcessorType::kBigCore>(tasks[0].app_data);
 
   // cleanup
   for (auto& task : tasks) {
@@ -75,11 +90,14 @@ void tmp() {
   }
 }
 
+// ---------------------------------------------------------------------
+// Main
+// ---------------------------------------------------------------------
+
 int main(int argc, char** argv) {
   parse_args(argc, argv);
 
-  spdlog::set_level(spdlog::level::debug);
-
   tmp();
+
   return 0;
 }
