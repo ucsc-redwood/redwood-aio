16803e1df6dae899d3962fcd523ae8f7f67aff8b
Yanwen Xu <xuyanwen2012@gmail.com>
2025-02-13 21:48:25 -0800

    s

--- play/try-cifar-dense-vk/main.cpp
+++ play/try-cifar-dense-vk/main.cpp
@@ -14,89 +14,97 @@
 
 void run_dense_vk() {
   auto mr = cifar_dense::vulkan::Singleton::getInstance().get_mr();
-  cifar_dense::AppData app_data(mr);
+  auto app_data = std::make_unique<cifar_dense::AppData>(mr);
 
-  cifar_dense::vulkan::Singleton::getInstance().process_stage_1(app_data);
-  cifar_dense::vulkan::Singleton::getInstance().process_stage_2(app_data);
-  cifar_dense::vulkan::Singleton::getInstance().process_stage_3(app_data);
-  cifar_dense::vulkan::Singleton::getInstance().process_stage_4(app_data);
-  cifar_dense::vulkan::Singleton::getInstance().process_stage_5(app_data);
-  cifar_dense::vulkan::Singleton::getInstance().process_stage_6(app_data);
-  cifar_dense::vulkan::Singleton::getInstance().process_stage_7(app_data);
-  cifar_dense::vulkan::Singleton::getInstance().process_stage_8(app_data);
-  cifar_dense::vulkan::Singleton::getInstance().process_stage_9(app_data);
+  cifar_dense::vulkan::Singleton::getInstance().process_stage_1(*app_data);
+  cifar_dense::vulkan::Singleton::getInstance().process_stage_2(*app_data);
+  cifar_dense::vulkan::Singleton::getInstance().process_stage_3(*app_data);
+  cifar_dense::vulkan::Singleton::getInstance().process_stage_4(*app_data);
+  cifar_dense::vulkan::Singleton::getInstance().process_stage_5(*app_data);
+  cifar_dense::vulkan::Singleton::getInstance().process_stage_6(*app_data);
+  cifar_dense::vulkan::Singleton::getInstance().process_stage_7(*app_data);
+  cifar_dense::vulkan::Singleton::getInstance().process_stage_8(*app_data);
+  cifar_dense::vulkan::Singleton::getInstance().process_stage_9(*app_data);
 
-  const auto max_index = cifar_dense::arg_max(app_data.u_linear_out.data());
+  const auto max_index = cifar_dense::arg_max(app_data->u_linear_out.data());
 
   std::cout << "Dense VK: ";
   cifar_dense::print_prediction(max_index);
+
+  app_data.reset();
 }
 
 void run_dense_omp() {
   auto mr = std::pmr::new_delete_resource();
-  cifar_dense::AppData app_data(mr);
+  auto app_data = std::make_unique<cifar_dense::AppData>(mr);
 
-  cifar_dense::omp::process_stage_1(app_data);
-  cifar_dense::omp::process_stage_2(app_data);
-  cifar_dense::omp::process_stage_3(app_data);
-  cifar_dense::omp::process_stage_4(app_data);
-  cifar_dense::omp::process_stage_5(app_data);
-  cifar_dense::omp::process_stage_6(app_data);
-  cifar_dense::omp::process_stage_7(app_data);
-  cifar_dense::omp::process_stage_8(app_data);
-  cifar_dense::omp::process_stage_9(app_data);
+  cifar_dense::omp::process_stage_1(*app_data);
+  cifar_dense::omp::process_stage_2(*app_data);
+  cifar_dense::omp::process_stage_3(*app_data);
+  cifar_dense::omp::process_stage_4(*app_data);
+  cifar_dense::omp::process_stage_5(*app_data);
+  cifar_dense::omp::process_stage_6(*app_data);
+  cifar_dense::omp::process_stage_7(*app_data);
+  cifar_dense::omp::process_stage_8(*app_data);
+  cifar_dense::omp::process_stage_9(*app_data);
 
-  const auto max_index = cifar_dense::arg_max(app_data.u_linear_out.data());
+  const auto max_index = cifar_dense::arg_max(app_data->u_linear_out.data());
 
   std::cout << "Dense OMP: ";
   cifar_dense::print_prediction(max_index);
+
+  app_data.reset();
 }
 
 void run_sparse_vk() {
   auto mr = cifar_sparse::vulkan::Singleton::getInstance().get_mr();
-  cifar_sparse::AppData app_data(mr);
+  auto app_data = std::make_unique<cifar_sparse::AppData>(mr);
 
-  cifar_sparse::vulkan::Singleton::getInstance().process_stage_1(app_data);
-  cifar_sparse::vulkan::Singleton::getInstance().process_stage_2(app_data);
-  cifar_sparse::vulkan::Singleton::getInstance().process_stage_3(app_data);
-  cifar_sparse::vulkan::Singleton::getInstance().process_stage_4(app_data);
-  cifar_sparse::vulkan::Singleton::getInstance().process_stage_5(app_data);
-  cifar_sparse::vulkan::Singleton::getInstance().process_stage_6(app_data);
-  cifar_sparse::vulkan::Singleton::getInstance().process_stage_7(app_data);
-  cifar_sparse::vulkan::Singleton::getInstance().process_stage_8(app_data);
-  cifar_sparse::vulkan::Singleton::getInstance().process_stage_9(app_data);
+  cifar_sparse::vulkan::Singleton::getInstance().process_stage_1(*app_data);
+  cifar_sparse::vulkan::Singleton::getInstance().process_stage_2(*app_data);
+  cifar_sparse::vulkan::Singleton::getInstance().process_stage_3(*app_data);
+  cifar_sparse::vulkan::Singleton::getInstance().process_stage_4(*app_data);
+  cifar_sparse::vulkan::Singleton::getInstance().process_stage_5(*app_data);
+  cifar_sparse::vulkan::Singleton::getInstance().process_stage_6(*app_data);
+  cifar_sparse::vulkan::Singleton::getInstance().process_stage_7(*app_data);
+  cifar_sparse::vulkan::Singleton::getInstance().process_stage_8(*app_data);
+  cifar_sparse::vulkan::Singleton::getInstance().process_stage_9(*app_data);
 
-  const auto max_index = cifar_sparse::arg_max(app_data.u_linear_output.data());
+  const auto max_index = cifar_sparse::arg_max(app_data->u_linear_output.data());
 
   std::cout << "Sparse VK: ";
   cifar_sparse::print_prediction(max_index);
+
+  app_data.reset();
 }
 
 void run_sparse_omp() {
   auto mr = std::pmr::new_delete_resource();
-  cifar_sparse::AppData app_data(mr);
+  auto app_data = std::make_unique<cifar_sparse::AppData>(mr);
 
-  cifar_sparse::omp::process_stage_1(app_data);
-  cifar_sparse::omp::process_stage_2(app_data);
-  cifar_sparse::omp::process_stage_3(app_data);
-  cifar_sparse::omp::process_stage_4(app_data);
-  cifar_sparse::omp::process_stage_5(app_data);
-  cifar_sparse::omp::process_stage_6(app_data);
-  cifar_sparse::omp::process_stage_7(app_data);
-  cifar_sparse::omp::process_stage_8(app_data);
-  cifar_sparse::omp::process_stage_9(app_data);
+  cifar_sparse::omp::process_stage_1(*app_data);
+  cifar_sparse::omp::process_stage_2(*app_data);
+  cifar_sparse::omp::process_stage_3(*app_data);
+  cifar_sparse::omp::process_stage_4(*app_data);
+  cifar_sparse::omp::process_stage_5(*app_data);
+  cifar_sparse::omp::process_stage_6(*app_data);
+  cifar_sparse::omp::process_stage_7(*app_data);
+  cifar_sparse::omp::process_stage_8(*app_data);
+  cifar_sparse::omp::process_stage_9(*app_data);
 
-  const auto max_index = cifar_sparse::arg_max(app_data.u_linear_output.data());
+  const auto max_index = cifar_sparse::arg_max(app_data->u_linear_output.data());
 
   std::cout << "Sparse OMP: ";
   cifar_sparse::print_prediction(max_index);
+
+  app_data.reset();
 }
 
 int main(int argc, char** argv) {
-  parse_args(argc, argv);
-
-  run_dense_vk();
+   
+  // run_dense_vk();
   run_dense_omp();
+
   run_sparse_vk();
   run_sparse_omp();
 
