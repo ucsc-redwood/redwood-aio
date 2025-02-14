2644ff75867ecc622ed2207f102311bbb3b2c4ff
Yanwen Xu <xuyanwen2012@gmail.com>
2025-02-13 17:30:27 -0800

    save

--- pipe/cifar-dense-vk/main.cpp
+++ pipe/cifar-dense-vk/main.cpp
@@ -1,4 +1,5 @@
 
+#include <climits>
 #include <cstdint>
 
 #include "affinity.hpp"
@@ -203,6 +204,95 @@ void stage_group_C(moodycamel::ConcurrentQueue<Task>& q_BC, std::vector<Task>& o
 
 }  // namespace instance_1
 
+namespace instance_2 {
+// --- Valid Execution Schedule #81 ---
+// Schedule Report:
+//   Chunk 1: Hardware = little, Threads = 4
+//     Stage 1: 4.24 ms
+//     Chunk Total Time: 4.24 ms
+//   Chunk 2: Hardware = big, Threads = 2
+//     Stage 2: 0.153 ms
+//     Stage 3: 23.8 ms
+//     Stage 4: 0.118 ms
+//     Chunk Total Time: 24.070999999999998 ms
+//   Chunk 3: Hardware = gpu, Threads = 1
+//     Stage 5: 9.02 ms
+//     Stage 6: 12.4 ms
+//     Stage 7: 9.66 ms
+//     Chunk Total Time: 31.080000000000002 ms
+//   Chunk 4: Hardware = medium, Threads = 2
+//     Stage 8: 0.046 ms
+//     Stage 9: 0.025 ms
+//     Chunk Total Time: 0.07100000000000001 ms
+// Pipeline Total Time: 59.462 ms
+// Max (Slowest) Chunk Time: 31.080000000000002 ms
+
+void stage_group_A(std::vector<Task>& in_tasks, moodycamel::ConcurrentQueue<Task>& q_AB) {
+  for (auto& task : in_tasks) {
+    // ---------
+    run_stages<1, 1, ProcessorType::kLittleCore, 4>(task.app_data);
+    // ---------
+
+    q_AB.enqueue(task);
+  }
+
+  done = true;
+}
+
+void stage_group_B(moodycamel::ConcurrentQueue<Task>& q_AB,
+                   moodycamel::ConcurrentQueue<Task>& q_BC) {
+  while (!done) {
+    Task task;
+
+    if (q_AB.try_dequeue(task)) {
+      // ---------
+      run_stages<2, 4, ProcessorType::kBigCore, 2>(task.app_data);
+      // ---------
+
+      q_BC.enqueue(task);
+    } else {
+      // No task available, yield to avoid busy-waiting
+      std::this_thread::yield();
+    }
+  }
+}
+
+void stage_group_C(moodycamel::ConcurrentQueue<Task>& q_BC,
+                   moodycamel::ConcurrentQueue<Task>& q_CD) {
+  while (!done) {
+    Task task;
+
+    if (q_BC.try_dequeue(task)) {
+      // ---------
+      cifar_dense::vulkan::Singleton::getInstance().run_stage<5>(*task.app_data);
+      cifar_dense::vulkan::Singleton::getInstance().run_stage<6>(*task.app_data);
+      cifar_dense::vulkan::Singleton::getInstance().run_stage<7>(*task.app_data);
+      // ---------
+
+      q_CD.enqueue(task);
+    } else {
+      // No task available, yield to avoid busy-waiting
+      std::this_thread::yield();
+    }
+  }
+}
+
+void stage_group_D(moodycamel::ConcurrentQueue<Task>& q_CD, std::vector<Task>& out_tasks) {
+  while (!done) {
+    Task task;
+
+    if (q_CD.try_dequeue(task)) {
+      // ---------
+      run_stages<8, 9, ProcessorType::kMediumCore, 1>(task.app_data);
+      // ---------
+
+      out_tasks.push_back(task);
+    }
+  }
+}
+
+}  // namespace instance_2
+
 }  // namespace device_3A021JEHN02756
 
 // ---------------------------------------------------------------------
@@ -284,7 +374,7 @@ void stage_group_B(moodycamel::ConcurrentQueue<Task>& q_AB, std::vector<Task>& o
 // ---------------------------------------------------------------------
 
 void tmp() {
-  auto tasks = init_tasks(10);
+  auto tasks = init_tasks(20);
   std::vector<Task> out_tasks;
   out_tasks.reserve(tasks.size());
 
@@ -354,6 +444,56 @@ void tmp() {
 }
 
 // ---------------------------------------------------------------------
+// Pipeline Instance (Best)
+// ---------------------------------------------------------------------
+
+void run_best() {
+  auto tasks = init_tasks(20);
+  std::vector<Task> out_tasks;
+  out_tasks.reserve(tasks.size());
+
+  auto start = std::chrono::high_resolution_clock::now();
+
+  if (g_device_id == "3A021JEHN02756") {
+    // Little cores: 0 1 2 3
+    // Mid cores: 4 5
+    // Big cores: 6 7
+
+    moodycamel::ConcurrentQueue<Task> q_AB;
+    moodycamel::ConcurrentQueue<Task> q_BC;
+    moodycamel::ConcurrentQueue<Task> q_CD;
+
+    std::thread t_A(
+        device_3A021JEHN02756::instance_2::stage_group_A, std::ref(tasks), std::ref(q_AB));
+    std::thread t_B(
+        device_3A021JEHN02756::instance_2::stage_group_B, std::ref(q_AB), std::ref(q_BC));
+    std::thread t_C(
+        device_3A021JEHN02756::instance_2::stage_group_C, std::ref(q_BC), std::ref(q_CD));
+    std::thread t_D(
+        device_3A021JEHN02756::instance_2::stage_group_D, std::ref(q_CD), std::ref(out_tasks));
+
+    t_A.join();
+    t_B.join();
+    t_C.join();
+    t_D.join();
+
+  } else if (g_device_id == "9b034f1b") {
+    exit(0);
+  } else if (g_device_id == "ce0717178d7758b00b7e") {
+    exit(0);
+  }
+
+  auto end = std::chrono::high_resolution_clock::now();
+
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+  double avg_time = duration.count() / static_cast<double>(tasks.size());
+  std::cout << "Pipeline: Average time per iteration: " << avg_time << " us" << "\t "
+            << avg_time / 1000.0 << " ms" << std::endl;
+
+  cleanup(tasks);
+}
+
+// ---------------------------------------------------------------------
 // Baseline
 // ---------------------------------------------------------------------
 
@@ -398,6 +538,7 @@ int main(int argc, char** argv) {
   spdlog::set_level(spdlog::level::off);
 
   tmp();
+  run_best();
   run_baseline();
 
   return 0;
