a32ff015839e7374d09f335d65edc6947946ba9f
Yanwen Xu <xuyanwen2012@gmail.com>
2025-02-15 11:51:19 -0800

    adding generated code

---
+++ pipe/cifar-dense-vk/generated-code/3A021JEHN02756_CifarDense_schedule_052.hpp
@@ -0,0 +1,23 @@
+// Auto-generated code for schedule: 3A021JEHN02756_CifarDense_schedule_052
+// Device ID: 3A021JEHN02756
+
+#pragma once
+
+#include <vector>
+#include "../task.hpp"
+#include "third-party/concurrentqueue.h"
+
+namespace device_3A021JEHN02756 {
+namespace CifarDense_schedule_052 {
+
+constexpr const char* kScheduleId = "3A021JEHN02756_CifarDense_schedule_052";
+
+void stage_group_chunk1(std::vector<Task>& in_tasks, moodycamel::ConcurrentQueue<Task>& out_q);
+void stage_group_chunk2(moodycamel::ConcurrentQueue<Task>& in_q, moodycamel::ConcurrentQueue<Task>& out_q);
+void stage_group_chunk3(moodycamel::ConcurrentQueue<Task>& in_q, moodycamel::ConcurrentQueue<Task>& out_q);
+void stage_group_chunk4(moodycamel::ConcurrentQueue<Task>& in_q, std::vector<Task>& out_tasks);
+
+void run_pipeline(std::vector<Task>& tasks, std::vector<Task>& out_tasks);
+
+}  // namespace CifarDense_schedule_052
+}  // namespace device_3A021JEHN02756
