d80372330cce789cbc32f8fa16bc87b9afe0d234
Yanwen Xu <xuyanwen2012@gmail.com>
2025-02-18 19:05:43 -0800

    improve tests

--- tests/test_omp_tree.cpp
+++ tests/test_omp_tree.cpp
@@ -1,20 +1,27 @@
 #include <gtest/gtest.h>
+#include <omp.h>
 
 #include <memory_resource>
+#include <thread>
 
 #include "builtin-apps/tree/omp/tree_kernel.hpp"
 #include "tests/verify_tree.hpp"
+
+#define PREPARE_APPDATA                                       \
+  const auto n_threads = std::thread::hardware_concurrency(); \
+  auto mr = std::pmr::new_delete_resource();                  \
+  tree::AppData appdata(mr);                                  \
+  tree::omp::TempStorage tmp_storage(n_threads, n_threads);
 
 // ----------------------------------------------------------------------------
 // Stage 1
 // ----------------------------------------------------------------------------
 
 TEST(OMP_Tree, Stage1) {
-  auto mr = std::pmr::new_delete_resource();
-  tree::AppData appdata(mr);
+  PREPARE_APPDATA;
 
 #pragma omp parallel
-  { tree::omp::run_stage<1>(appdata); }
+  { tree::omp::run_stage<1>(appdata, tmp_storage); }
 
   test_tree::verify_stage_1(appdata);
 }
@@ -24,16 +31,109 @@ TEST(OMP_Tree, Stage1) {
 // ----------------------------------------------------------------------------
 
 TEST(OMP_Tree, Stage2) {
-  auto mr = std::pmr::new_delete_resource();
-  tree::AppData appdata(mr);
+  PREPARE_APPDATA;
 
-  tree::omp::run_stage<1>(appdata);
+  tree::omp::run_stage<1>(appdata, tmp_storage);
 
 #pragma omp parallel
-  { tree::omp::run_stage<2>(appdata); }
+  { tree::omp::run_stage<2>(appdata, tmp_storage); }
 
   test_tree::verify_stage_2(appdata);
 }
+
+// ----------------------------------------------------------------------------
+// Stage 3
+// ----------------------------------------------------------------------------
+
+TEST(OMP_Tree, Stage3) {
+  PREPARE_APPDATA;
+
+  tree::omp::run_stage<1>(appdata, tmp_storage);
+  tree::omp::run_stage<2>(appdata, tmp_storage);
+
+#pragma omp parallel
+  { tree::omp::run_stage<3>(appdata, tmp_storage); }
+
+  test_tree::verify_stage_3(appdata);
+}
+
+// ----------------------------------------------------------------------------
+// Stage 4
+// ----------------------------------------------------------------------------
+
+TEST(OMP_Tree, Stage4) {
+  PREPARE_APPDATA;
+
+  tree::omp::run_stage<1>(appdata, tmp_storage);
+  tree::omp::run_stage<2>(appdata, tmp_storage);
+  tree::omp::run_stage<3>(appdata, tmp_storage);
+
+#pragma omp parallel
+  { tree::omp::run_stage<4>(appdata, tmp_storage); }
+
+  test_tree::verify_stage_4(appdata);
+}
+
+// ----------------------------------------------------------------------------
+// Stage 5
+// ----------------------------------------------------------------------------
+
+TEST(OMP_Tree, Stage5) {
+  PREPARE_APPDATA;
+
+  tree::omp::run_stage<1>(appdata, tmp_storage);
+  tree::omp::run_stage<2>(appdata, tmp_storage);
+  tree::omp::run_stage<3>(appdata, tmp_storage);
+  tree::omp::run_stage<4>(appdata, tmp_storage);
+
+#pragma omp parallel
+  { tree::omp::run_stage<5>(appdata, tmp_storage); }
+
+  test_tree::verify_stage_5(appdata);
+}
+
+// ----------------------------------------------------------------------------
+// Stage 6
+// ----------------------------------------------------------------------------
+
+TEST(OMP_Tree, Stage6) {
+  PREPARE_APPDATA;
+
+  tree::omp::run_stage<1>(appdata, tmp_storage);
+  tree::omp::run_stage<2>(appdata, tmp_storage);
+  tree::omp::run_stage<3>(appdata, tmp_storage);
+  tree::omp::run_stage<4>(appdata, tmp_storage);
+  tree::omp::run_stage<5>(appdata, tmp_storage);
+
+#pragma omp parallel
+  { tree::omp::run_stage<6>(appdata, tmp_storage); }
+
+  test_tree::verify_stage_6(appdata);
+}
+
+// ----------------------------------------------------------------------------
+// Stage 7
+// ----------------------------------------------------------------------------
+
+TEST(OMP_Tree, Stage7) {
+  PREPARE_APPDATA;
+
+  tree::omp::run_stage<1>(appdata, tmp_storage);
+  tree::omp::run_stage<2>(appdata, tmp_storage);
+  tree::omp::run_stage<3>(appdata, tmp_storage);
+  tree::omp::run_stage<4>(appdata, tmp_storage);
+  tree::omp::run_stage<5>(appdata, tmp_storage);
+  tree::omp::run_stage<6>(appdata, tmp_storage);
+
+#pragma omp parallel
+  { tree::omp::run_stage<7>(appdata, tmp_storage); }
+
+  test_tree::verify_stage_7(appdata);
+}
+
+// ----------------------------------------------------------------------------
+// Main function for running tests
+// ----------------------------------------------------------------------------
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
--- tests/verify_tree.hpp
+++ tests/verify_tree.hpp
@@ -2,6 +2,8 @@
 
 #include <gtest/gtest.h>
 
+#include <algorithm>
+
 #include "builtin-apps/tree/tree_appdata.hpp"
 
 namespace test_tree {
@@ -11,10 +13,7 @@ namespace test_tree {
 // ----------------------------------------------------------------------------
 
 inline void verify_stage_1(tree::AppData &appdata) {
-  // Verify results are non-zero
-  for (auto i = 0u; i < appdata.get_n_input(); i++) {
-    EXPECT_NE(appdata.u_morton_keys_s1[i], 0) << "Morton code at index " << i << " is zero";
-  }
+  EXPECT_TRUE(std::ranges::all_of(appdata.u_morton_keys_s1, [](auto x) { return x != 0; }));
 }
 
 // ----------------------------------------------------------------------------
@@ -22,15 +21,84 @@ inline void verify_stage_1(tree::AppData &appdata) {
 // ----------------------------------------------------------------------------
 
 inline void verify_stage_2(tree::AppData &appdata) {
-  // Verify results are non-zero and sorted
-  for (auto i = 0u; i < appdata.get_n_input(); i++) {
-    EXPECT_NE(appdata.u_morton_keys_sorted_s2[i], 0)
-        << "Sorted Morton code at index " << i << " is zero";
+  EXPECT_TRUE(std::ranges::all_of(appdata.u_morton_keys_sorted_s2, [](auto x) { return x != 0; }));
+  EXPECT_TRUE(std::ranges::is_sorted(appdata.u_morton_keys_sorted_s2));
+}
 
-    if (i > 0) {
-      EXPECT_LT(appdata.u_morton_keys_sorted_s2[i], appdata.u_morton_keys_sorted_s2[i - 1])
-          << "Sorted Morton code at index " << i << " is not less than the previous code";
-    }
+// ----------------------------------------------------------------------------
+// Stage 3
+// ----------------------------------------------------------------------------
+
+inline void verify_stage_3(tree::AppData &appdata) {
+  std::vector<uint32_t> expected_unique_morton_keys(appdata.get_n_input());
+
+  const auto last = std::unique_copy(appdata.u_morton_keys_sorted_s2.data(),
+                                     appdata.u_morton_keys_sorted_s2.data() + appdata.get_n_input(),
+                                     expected_unique_morton_keys.data());
+  const auto num_unique = std::distance(expected_unique_morton_keys.data(), last);
+
+  EXPECT_EQ(appdata.get_n_unique(), num_unique);
+  EXPECT_EQ(appdata.get_n_brt_nodes(), num_unique - 1);
+
+  for (uint32_t i = 0; i < num_unique; ++i) {
+    EXPECT_EQ(appdata.u_morton_keys_unique_s3[i], expected_unique_morton_keys[i]);
+  }
+}
+
+// ----------------------------------------------------------------------------
+// Stage 4
+// ----------------------------------------------------------------------------
+
+inline void verify_stage_4(tree::AppData &appdata) {
+  // Verify BRT parents are non-zero
+  ASSERT_GT(appdata.get_n_brt_nodes(), 0);
+
+  ASSERT_EQ(appdata.u_brt_parents_s4[0], 0);
+  for (uint32_t i = 1; i < appdata.get_n_brt_nodes(); ++i) {
+    EXPECT_GE(appdata.u_brt_parents_s4[i], 0);
+  }
+}
+
+// ----------------------------------------------------------------------------
+// Stage 5
+// ----------------------------------------------------------------------------
+
+inline void verify_stage_5(tree::AppData &appdata) {
+  // Verify not all are zeros
+  bool all_zeros = std::ranges::all_of(appdata.u_edge_count_s5, [](int x) { return x == 0; });
+  EXPECT_FALSE(all_zeros);
+}
+
+// ----------------------------------------------------------------------------
+// Stage 6
+// ----------------------------------------------------------------------------
+
+inline void verify_stage_6(tree::AppData &appdata) {
+  // Verify not all are zeros
+  bool all_zeros = std::ranges::all_of(appdata.u_edge_offset_s6, [](int x) { return x == 0; });
+  EXPECT_FALSE(all_zeros);
+
+  // Verify edge offsets are monotonically increasing
+  for (uint32_t i = 1; i < appdata.get_n_brt_nodes(); ++i) {
+    EXPECT_LE(appdata.u_edge_offset_s6[i - 1], appdata.u_edge_offset_s6[i]);
+  }
+}
+
+// ----------------------------------------------------------------------------
+// Stage 7
+// ----------------------------------------------------------------------------
+
+inline void verify_stage_7(tree::AppData &appdata) {
+  // Verify not all are zeros
+  bool all_zeros =
+      std::ranges::all_of(appdata.u_oct_child_node_mask_s7, [](int x) { return x == 0; });
+  EXPECT_FALSE(all_zeros);
+
+  // Verify octree nodes
+  ASSERT_GT(appdata.get_n_octree_nodes(), 0);
+  for (uint32_t i = 0; i < appdata.get_n_octree_nodes(); ++i) {
+    EXPECT_GE(appdata.u_oct_child_node_mask_s7[i], 0);
+    EXPECT_LE(appdata.u_oct_child_node_mask_s7[i], 255);  // 8-bit mask
   }
 }
 
