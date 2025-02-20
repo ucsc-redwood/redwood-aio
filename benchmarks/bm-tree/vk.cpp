3a19fe9bd0c287f37edcfa7cbe25105dc1b58c26
Yanwen Xu <xuyanwen2012@gmail.com>
2025-02-19 21:02:24 -0800

    tree vk update

--- benchmarks/bm-tree/vk.cpp
+++ benchmarks/bm-tree/vk.cpp
@@ -169,7 +169,7 @@ BENCHMARK_DEFINE_F(VK_Tree, Stage7)
   }
 }
 
-BENCHMARK_REGISTER_F(VK_Tree, Stage7)->Unit(benchmark::kMillisecond)->Iterations(1);
+BENCHMARK_REGISTER_F(VK_Tree, Stage7)->Unit(benchmark::kMillisecond);
 
 // ----------------------------------------------------------------
 // Main
