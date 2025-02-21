3296db44742f6ad18dd4a3bbaec6f6fa2e72d150
Yanwen Xu <xuyanwen2012@gmail.com>
2025-02-21 14:59:24 -0800

    s

--- pipe/try-tree-vk/main.cpp
+++ pipe/try-tree-vk/main.cpp
@@ -10,116 +10,6 @@
 #include "builtin-apps/tree/vulkan/dispatchers.hpp"
 
 // ---------------------------------------------------------------------
-// New Design
-// ---------------------------------------------------------------------
-
-template <int Stage>
-concept ValidStage = (Stage >= 1) && (Stage <= 7);
-
-template <int Start, int End>
-concept ValidStageRange = ValidStage<Start> && ValidStage<End> && (Start <= End);
-
-// Helper function that unfolds the stage calls.
-template <int Start, int... Is>
-void run_cpu_stages_impl(tree::AppData* app_data,
-                         tree::omp::TmpStorage* tmp_storage,
-                         std::integer_sequence<int, Is...>) {
-  // Expand the calls: run_stage<Start + 0>(), run_stage<Start + 1>(), ...
-  (tree::omp::run_stage<Start + Is>(*app_data, *tmp_storage), ...);
-}
-
-// Main interface
-template <int Start, int End, ProcessorType processor_type, int n_threads>
-  requires ValidStageRange<Start, End>
-void run_cpu_stages(tree::AppData* app_data, tree::omp::TmpStorage* tmp_storage) {
-  // Bind to the selected cores
-  if constexpr (processor_type == ProcessorType::kLittleCore) {
-    bind_thread_to_cores(g_little_cores);
-  } else if constexpr (processor_type == ProcessorType::kMediumCore) {
-    bind_thread_to_cores(g_medium_cores);
-  } else if constexpr (processor_type == ProcessorType::kBigCore) {
-    bind_thread_to_cores(g_big_cores);
-  }
-
-#pragma omp parallel num_threads(n_threads)
-  {
-    // Generate the sequence [0, 1, 2, ..., (End-Start)]
-    // and expand the calls.
-    run_cpu_stages_impl<Start>(
-        app_data, tmp_storage, std::make_integer_sequence<int, End - Start + 1>{});
-  }
-}
-
-// Helper function that unfolds the stage calls.
-template <int Start, int... Is>
-void run_gpu_stages_impl(tree::AppData* app_data,
-                         tree::vulkan::TmpStorage* tmp_storage,
-                         std::integer_sequence<int, Is...>) {
-  // Expand the calls: run_stage<Start + 0>(), run_stage<Start + 1>(), ...
-  (tree::vulkan::Singleton::getInstance().run_stage<Start + Is>(*app_data, *tmp_storage), ...);
-}
-
-// Main interface
-template <int Start, int End>
-  requires ValidStageRange<Start, End>
-void run_gpu_stages(tree::AppData* app_data, tree::vulkan::TmpStorage* tmp_storage) {
-  // Generate the sequence [0, 1, 2, ..., (End-Start)]
-  // and expand the calls.
-  run_gpu_stages_impl<Start>(
-      app_data, tmp_storage, std::make_integer_sequence<int, End - Start + 1>{});
-}
-
-// ---------------------------------------------------------------------
-// Old working design
-// ---------------------------------------------------------------------
-
-// template <int start_stage, int end_stage, ProcessorType processor_type, int num_threads>
-//   requires(start_stage <= end_stage) && (start_stage >= 1) && (end_stage <= 7)
-// void run_stages(tree::AppData* app_data, tree::omp::TmpStorage* omp_tmp_storage) {
-// #pragma omp parallel num_threads(num_threads)
-//   {
-//     // Bind to core if needed:
-//     if constexpr (processor_type == ProcessorType::kLittleCore) {
-//       bind_thread_to_cores(g_little_cores);
-//     } else if constexpr (processor_type == ProcessorType::kMediumCore) {
-//       bind_thread_to_cores(g_medium_cores);
-//     } else if constexpr (processor_type == ProcessorType::kBigCore) {
-//       bind_thread_to_cores(g_big_cores);
-//     }
-
-//     // Generate a compile-time sequence for the range [start_stage, end_stage]
-//     []<std::size_t... I>(
-//         std::index_sequence<I...>, tree::AppData& data, tree::omp::TmpStorage& omp_tmp_storage) {
-//       // Each I is offset by (start_stage - 1)
-//       ((tree::omp::run_stage<start_stage + I>(data, omp_tmp_storage)), ...);
-//     }(std::make_index_sequence<end_stage - start_stage + 1>{}, *app_data, *omp_tmp_storage);
-//   }
-// }
-
-// /**
-//  * @brief Runs stages of the CIFAR dense network on GPU using Vulkan
-//  *
-//  * @tparam start_stage First stage to execute (must be >= 1)
-//  * @tparam end_stage Last stage to execute (must be <= 9)
-//  * @param app_data Pointer to application data containing network state
-//  *
-//  * This template function executes the specified range of network stages on the GPU using Vulkan.
-//  * The stages are run in sequence using compile-time unrolling.
-//  */
-// template <int start_stage, int end_stage>
-//   requires(start_stage <= end_stage) && (start_stage >= 1) && (end_stage <= 7)
-// void run_gpu_stages(tree::AppData* app_data, tree::vulkan::TmpStorage* vulkan_tmp_storage) {
-//   // Generate a compile-time sequence for the range [start_stage, end_stage]
-//   []<std::size_t... I>(std::index_sequence<I...>,
-//                        tree::AppData& data,
-//                        tree::vulkan::TmpStorage& vulkan_tmp_storage) {
-//     ((tree::vulkan::Singleton::getInstance().run_stage<start_stage + I>(data,
-//     vulkan_tmp_storage)),
-//      ...);
-//   }(std::make_index_sequence<end_stage - start_stage + 1>{}, *app_data, *vulkan_tmp_storage);
-// }
-
-// ---------------------------------------------------------------------
 // Task structure
 // ---------------------------------------------------------------------
 struct Task {
@@ -176,6 +66,116 @@ void cleanup(std::queue<Task>& tasks) {
 }
 
 // ---------------------------------------------------------------------
+// New Design
+// ---------------------------------------------------------------------
+
+template <int Stage>
+concept ValidStage = (Stage >= 1) && (Stage <= 7);
+
+template <int Start, int End>
+concept ValidStageRange = ValidStage<Start> && ValidStage<End> && (Start <= End);
+
+// Helper function that unfolds the stage calls.
+template <int Start, int... Is>
+void run_cpu_stages_impl(tree::AppData* app_data,
+                         tree::omp::TmpStorage* tmp_storage,
+                         std::integer_sequence<int, Is...>) {
+  // Expand the calls: run_stage<Start + 0>(), run_stage<Start + 1>(), ...
+  (tree::omp::run_stage<Start + Is>(*app_data, *tmp_storage), ...);
+}
+
+// Main interface
+template <int Start, int End, ProcessorType processor_type, int n_threads>
+  requires ValidStageRange<Start, End>
+void run_cpu_stages(Task& task) {
+  // Bind to the selected cores
+  if constexpr (processor_type == ProcessorType::kLittleCore) {
+    bind_thread_to_cores(g_little_cores);
+  } else if constexpr (processor_type == ProcessorType::kMediumCore) {
+    bind_thread_to_cores(g_medium_cores);
+  } else if constexpr (processor_type == ProcessorType::kBigCore) {
+    bind_thread_to_cores(g_big_cores);
+  }
+
+#pragma omp parallel num_threads(n_threads)
+  {
+    // Generate the sequence [0, 1, 2, ..., (End-Start)]
+    // and expand the calls.
+    run_cpu_stages_impl<Start>(
+        task.app_data, task.omp_tmp_storage, std::make_integer_sequence<int, End - Start + 1>{});
+  }
+}
+
+// Helper function that unfolds the stage calls.
+template <int Start, int... Is>
+void run_gpu_stages_impl(tree::AppData* app_data,
+                         tree::vulkan::TmpStorage* tmp_storage,
+                         std::integer_sequence<int, Is...>) {
+  // Expand the calls: run_stage<Start + 0>(), run_stage<Start + 1>(), ...
+  (tree::vulkan::Singleton::getInstance().run_stage<Start + Is>(*app_data, *tmp_storage), ...);
+}
+
+// Main interface
+template <int Start, int End>
+  requires ValidStageRange<Start, End>
+void run_gpu_stages(Task& task) {
+  // Generate the sequence [0, 1, 2, ..., (End-Start)]
+  // and expand the calls.
+  run_gpu_stages_impl<Start>(
+      task.app_data, task.vulkan_tmp_storage, std::make_integer_sequence<int, End - Start + 1>{});
+}
+
+// ---------------------------------------------------------------------
+// Old working design
+// ---------------------------------------------------------------------
+
+// template <int start_stage, int end_stage, ProcessorType processor_type, int num_threads>
+//   requires(start_stage <= end_stage) && (start_stage >= 1) && (end_stage <= 7)
+// void run_stages(tree::AppData* app_data, tree::omp::TmpStorage* omp_tmp_storage) {
+// #pragma omp parallel num_threads(num_threads)
+//   {
+//     // Bind to core if needed:
+//     if constexpr (processor_type == ProcessorType::kLittleCore) {
+//       bind_thread_to_cores(g_little_cores);
+//     } else if constexpr (processor_type == ProcessorType::kMediumCore) {
+//       bind_thread_to_cores(g_medium_cores);
+//     } else if constexpr (processor_type == ProcessorType::kBigCore) {
+//       bind_thread_to_cores(g_big_cores);
+//     }
+
+//     // Generate a compile-time sequence for the range [start_stage, end_stage]
+//     []<std::size_t... I>(
+//         std::index_sequence<I...>, tree::AppData& data, tree::omp::TmpStorage& omp_tmp_storage) {
+//       // Each I is offset by (start_stage - 1)
+//       ((tree::omp::run_stage<start_stage + I>(data, omp_tmp_storage)), ...);
+//     }(std::make_index_sequence<end_stage - start_stage + 1>{}, *app_data, *omp_tmp_storage);
+//   }
+// }
+
+// /**
+//  * @brief Runs stages of the CIFAR dense network on GPU using Vulkan
+//  *
+//  * @tparam start_stage First stage to execute (must be >= 1)
+//  * @tparam end_stage Last stage to execute (must be <= 9)
+//  * @param app_data Pointer to application data containing network state
+//  *
+//  * This template function executes the specified range of network stages on the GPU using Vulkan.
+//  * The stages are run in sequence using compile-time unrolling.
+//  */
+// template <int start_stage, int end_stage>
+//   requires(start_stage <= end_stage) && (start_stage >= 1) && (end_stage <= 7)
+// void run_gpu_stages(tree::AppData* app_data, tree::vulkan::TmpStorage* vulkan_tmp_storage) {
+//   // Generate a compile-time sequence for the range [start_stage, end_stage]
+//   []<std::size_t... I>(std::index_sequence<I...>,
+//                        tree::AppData& data,
+//                        tree::vulkan::TmpStorage& vulkan_tmp_storage) {
+//     ((tree::vulkan::Singleton::getInstance().run_stage<start_stage + I>(data,
+//     vulkan_tmp_storage)),
+//      ...);
+//   }(std::make_index_sequence<end_stage - start_stage + 1>{}, *app_data, *vulkan_tmp_storage);
+// }
+
+// ---------------------------------------------------------------------
 // Define a Schedule
 // ---------------------------------------------------------------------
 
@@ -191,7 +191,7 @@ void chunk1(std::queue<Task>& in_tasks, moodycamel::ConcurrentQueue<Task>& out_q
     }
 
     // ---------------------------------------------------------------------
-    run_cpu_stages<1, 3, ProcessorType::kBigCore, 2>(task.app_data, task.omp_tmp_storage);
+    run_cpu_stages<1, 3, ProcessorType::kBigCore, 2>(task);
     // ---------------------------------------------------------------------
 
     out_q.enqueue(task);
@@ -208,7 +208,7 @@ void chunk2(moodycamel::ConcurrentQueue<Task>& in_q, moodycamel::ConcurrentQueue
       }
 
       // ---------------------------------------------------------------------
-      run_cpu_stages<4, 6, ProcessorType::kLittleCore, 4>(task.app_data, task.omp_tmp_storage);
+      run_cpu_stages<4, 6, ProcessorType::kLittleCore, 4>(task);
       // ---------------------------------------------------------------------
 
       out_q.enqueue(task);
@@ -227,7 +227,7 @@ void chunk3(moodycamel::ConcurrentQueue<Task>& in_q, std::queue<Task>& out_tasks
       }
 
       // ---------------------------------------------------------------------
-      run_gpu_stages<7, 7>(task.app_data, task.vulkan_tmp_storage);
+      run_gpu_stages<7, 7>(task);
       // ---------------------------------------------------------------------
 
       out_tasks.push(task);
