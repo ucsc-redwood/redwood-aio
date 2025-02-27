5aa8e7b4466943e482ac15eb213ff8e92f3f1633
Yanwen Xu <xuyanwen2012@gmail.com>
2025-02-27 15:20:11 -0800

    save

---
+++ tests/test_cifar_dense_mixed_cu.cu
@@ -0,0 +1,91 @@
+#include <gtest/gtest.h>
+
+#include "builtin-apps/affinity.hpp"
+#include "builtin-apps/app.hpp"
+#include "builtin-apps/cifar-dense/cuda/dispatchers.cuh"
+#include "builtin-apps/cifar-dense/omp/dispatchers.hpp"
+#include "builtin-apps/common/cuda/cu_mem_resource.cuh"
+#include "builtin-apps/common/cuda/helpers.cuh"
+
+#define PREPARE_DATA                    \
+  auto mr = cuda::CudaMemoryResource(); \
+  cifar_dense::AppData appdata(&mr);    \
+  CUDA_CHECK(cudaDeviceSynchronize());
+
+// ----------------------------------------------------------------------------
+// Stages
+// ----------------------------------------------------------------------------
+
+TEST(CUDA_CIFAR_DENSE, Stage1_OMP_Then_CUDA) {
+  PREPARE_DATA;
+
+#pragma omp parallel num_threads(g_little_cores.size())
+  {
+    bind_thread_to_cores(g_little_cores);
+    cifar_dense::omp::run_stage<1>(appdata);
+  }
+
+  cifar_dense::cuda::run_stage<2>(appdata);
+  CUDA_CHECK(cudaDeviceSynchronize());
+
+  SUCCEED();
+}
+
+TEST(CUDA_CIFAR_DENSE, Stage12_OMP_Then_CUDA) {
+  PREPARE_DATA;
+
+#pragma omp parallel num_threads(g_little_cores.size())
+  {
+    bind_thread_to_cores(g_little_cores);
+    cifar_dense::omp::run_stage<1>(appdata);
+    cifar_dense::omp::run_stage<2>(appdata);
+  }
+
+  cifar_dense::cuda::run_stage<3>(appdata);
+  CUDA_CHECK(cudaDeviceSynchronize());
+
+  SUCCEED();
+}
+
+TEST(CUDA_CIFAR_DENSE, Stage123_OMP_Then_CUDA) {
+  PREPARE_DATA;
+
+#pragma omp parallel num_threads(g_little_cores.size())
+  {
+    bind_thread_to_cores(g_little_cores);
+    cifar_dense::omp::run_stage<1>(appdata);
+    cifar_dense::omp::run_stage<2>(appdata);
+    cifar_dense::omp::run_stage<3>(appdata);
+  }
+
+  cifar_dense::cuda::run_stage<4>(appdata);
+  CUDA_CHECK(cudaDeviceSynchronize());
+
+  SUCCEED();
+}
+
+TEST(CUDA_CIFAR_DENSE, Stage1234_OMP_Then_CUDA) {
+  PREPARE_DATA;
+
+#pragma omp parallel num_threads(g_little_cores.size())
+  {
+    bind_thread_to_cores(g_little_cores);
+    cifar_dense::omp::run_stage<1>(appdata);
+    cifar_dense::omp::run_stage<2>(appdata);
+    cifar_dense::omp::run_stage<3>(appdata);
+  }
+
+  cifar_dense::cuda::run_stage<5>(appdata);
+  CUDA_CHECK(cudaDeviceSynchronize());
+
+  SUCCEED();
+}
+
+int main(int argc, char **argv) {
+  parse_args(argc, argv);
+
+  spdlog::set_level(spdlog::level::debug);
+
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
