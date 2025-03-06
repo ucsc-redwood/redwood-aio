5a87cc284440a3f9dfafcfa0ff238a70c7a7d016
Yanwen Xu <xuyanwen2012@gmail.com>
2025-03-06 13:53:53 -0800

    s

---
+++ pipe/cifar-sparse-cu/xmake.lua
@@ -0,0 +1,22 @@
+-- Copyright (c) 2025 Yanwen Xu (yxu83@ucsc.edu). MIT License.
+
+target("pipe-cifar-sparse-cu")
+do
+	add_rules("pipe_config", "common_flags")
+
+	add_headerfiles({
+		"task.hpp",
+		"run_stages.hpp",
+		"../concepts.hpp",
+		"../templates.hpp",
+		"generated-code/all_schedules.hpp",
+	})
+
+	add_files({
+		"main.cu",
+		"task.cpp",
+	})
+
+	add_deps("builtin-apps", "builtin-apps-cuda")
+	add_cugencodes("native")
+end
---
+++ pipe/tree-cu/main.cu
@@ -0,0 +1,29 @@
+#include <spdlog/spdlog.h>
+
+#include "builtin-apps/app.hpp"
+#include "generated-code/all_schedules.hpp"
+
+// ---------------------------------------------------------------------
+// Main
+// ---------------------------------------------------------------------
+
+int main(int argc, char** argv) {
+  PARSE_ARGS_BEGIN;
+
+  int which_schedule = 1;
+  app.add_option("-s,--schedule", which_schedule, "Schedule ID")->required();
+
+  PARSE_ARGS_END;
+
+  spdlog::set_level(spdlog::level::from_str(g_spdlog_log_level));
+
+//   if (g_device_id == "jetson") {
+//     run_warmup<Task>(
+//         init_tasks_queue, device_jetson::get_run_pipeline_func(which_schedule), cleanup);
+
+//     run_pipelined_schedule<Task>(
+//         init_tasks_queue, device_jetson::get_run_pipeline_func(which_schedule), cleanup);
+//   }
+
+  return 0;
+}
