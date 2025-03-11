bee412217b3a8343de2afb0e4454668c600aa379
Yanwen Xu <xuyanwen2012@gmail.com>
2025-03-11 16:29:16 -0700

    s

--- pipe/try-tree-cu/main.cu
+++ pipe/try-tree-cu/main.cu
@@ -3,7 +3,6 @@
 
 #include "../templates.hpp"
 #include "builtin-apps/common/cuda/manager.cuh"
-#include "builtin-apps/conf.hpp"
 #include "run_stages.hpp"
 #include "task.hpp"
 
@@ -31,24 +30,28 @@ void program(const int num_tasks) {
 
   auto preallocated_data = init_appdata(&mgr.get_mr(), num_tasks);
 
-  // moodycamel::ConcurrentQueue<Task *> q_input = init_tasks(preallocated_data, &mgr);
-  // moodycamel::ConcurrentQueue<Task *> q_12;
-  // moodycamel::ConcurrentQueue<Task *> q_23;
+  moodycamel::ConcurrentQueue<Task *> q_input = init_tasks(preallocated_data, &mgr);
+  moodycamel::ConcurrentQueue<Task *> q_12;
+  moodycamel::ConcurrentQueue<Task *> q_23;
 
   auto start = std::chrono::high_resolution_clock::now();
 
   // ---------------------------------------------------------------------
+  // omp::run_multiple_stages<1, 3, ProcessorType::kBigCore, 8>(preallocated_data[0]);
 
-  // std::thread t1([&]() {
-  //   chunk<Task, cifar_dense::AppData>(
-  //       q_input, &q_12, omp::run_multiple_stages<1, 3, ProcessorType::kBigCore, 8>, mgr);
-  // });
+  std::thread t1([&]() {
+    chunk<Task, tree::AppData>(
+        q_input, &q_12, omp::run_multiple_stages<1, 2, ProcessorType::kBigCore, 8>, mgr);
+  });
+
+  t1.join();
+
   // std::thread t2([&]() {
-  //   chunk<Task, cifar_dense::AppData>(q_12, &q_23, cuda::run_multiple_stages<4, 6>, mgr);
+  //   chunk<Task, tree::AppData>(q_12, &q_23, cuda::run_multiple_stages<4, 6>, mgr);
   // });
   // std::thread t3([&]() {
-  //   chunk<Task, cifar_dense::AppData>(
-  //       q_23, nullptr, omp::run_multiple_stages<7, 9, ProcessorType::kLittleCore, 12>, mgr);
+  //   chunk<Task, tree::AppData>(
+  //       q_23, nullptr, omp::run_multiple_stages<7, 7, ProcessorType::kLittleCore, 12>, mgr);
   // });
 
   // t1.join();
--- pipe/try-tree-cu/run_stages.hpp
+++ pipe/try-tree-cu/run_stages.hpp
@@ -27,7 +27,7 @@ constexpr std::array<void (*)(tree::AppData &), 7> cpu_stages = {
 
 template <int Start, int End, ProcessorType PT, int NThreads>
   requires AllowedStage<Start> && AllowedStage<End> && (Start <= End)
-void run_multiple_stages(tree::AppData &data) {
+void run_multiple_stages(tree::AppData &data, cuda::CudaManager &) {
 #pragma omp parallel num_threads(NThreads)
   {
     // Bind to core
@@ -70,56 +70,12 @@ constexpr std::array<void (*)(tree::AppData &), 7> gpu_stages = {
 template <int Start, int End>
   requires AllowedStage<Start> && AllowedStage<End> && (Start <= End)
 void run_multiple_stages(tree::AppData &data, cuda::CudaManager &mgr) {
-  // CudaAttachSingle(data.u_conv1_bias.data());
-  // CudaAttachSingle(data.u_conv1_weights.data());
-  // CudaAttachSingle(data.u_conv1_out.data());
-  // CudaAttachSingle(data.u_pool1_out.data());
-  // CudaAttachSingle(data.u_conv2_bias.data());
-  // CudaAttachSingle(data.u_conv2_weights.data());
-  // CudaAttachSingle(data.u_conv2_out.data());
-  // CudaAttachSingle(data.u_pool2_out.data());
-  // CudaAttachSingle(data.u_conv3_bias.data());
-  // CudaAttachSingle(data.u_conv3_weights.data());
-  // CudaAttachSingle(data.u_conv3_out.data());
-  // CudaAttachSingle(data.u_conv4_bias.data());
-  // CudaAttachSingle(data.u_conv4_weights.data());
-  // CudaAttachSingle(data.u_conv4_out.data());
-  // CudaAttachSingle(data.u_conv5_bias.data());
-  // CudaAttachSingle(data.u_conv5_weights.data());
-  // CudaAttachSingle(data.u_conv5_out.data());
-  // CudaAttachSingle(data.u_pool3_out.data());
-  // CudaAttachSingle(data.u_linear_bias.data());
-  // CudaAttachSingle(data.u_linear_weights.data());
-  // CudaAttachSingle(data.u_linear_out.data());
-
 #pragma unroll
   for (int s = Start; s <= End; ++s) {
     gpu_stages[s - 1](data);
   }
 
   CheckCuda(cudaStreamSynchronize(mgr.get_stream()));
-
-  // CudaAttachHost(data.u_conv1_bias.data());
-  // CudaAttachHost(data.u_conv1_weights.data());
-  // CudaAttachHost(data.u_conv1_out.data());
-  // CudaAttachHost(data.u_pool1_out.data());
-  // CudaAttachHost(data.u_conv2_bias.data());
-  // CudaAttachHost(data.u_conv2_weights.data());
-  // CudaAttachHost(data.u_conv2_out.data());
-  // CudaAttachHost(data.u_pool2_out.data());
-  // CudaAttachHost(data.u_conv3_bias.data());
-  // CudaAttachHost(data.u_conv3_weights.data());
-  // CudaAttachHost(data.u_conv3_out.data());
-  // CudaAttachHost(data.u_conv4_bias.data());
-  // CudaAttachHost(data.u_conv4_weights.data());
-  // CudaAttachHost(data.u_conv4_out.data());
-  // CudaAttachHost(data.u_conv5_bias.data());
-  // CudaAttachHost(data.u_conv5_weights.data());
-  // CudaAttachHost(data.u_conv5_out.data());
-  // CudaAttachHost(data.u_pool3_out.data());
-  // CudaAttachHost(data.u_linear_bias.data());
-  // CudaAttachHost(data.u_linear_weights.data());
-  // CudaAttachHost(data.u_linear_out.data());
 }
 
 }  // namespace cuda
