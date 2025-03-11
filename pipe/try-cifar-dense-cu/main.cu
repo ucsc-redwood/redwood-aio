9da20eb375269137c5a41ecca130751ac89417b2
Yanwen Xu <xuyanwen2012@gmail.com>
2025-03-11 09:58:44 -0700

    s

--- pipe/try-cifar-dense-cu/main.cu
+++ pipe/try-cifar-dense-cu/main.cu
@@ -7,46 +7,7 @@
 #include "run_stages.hpp"
 #include "task.hpp"
 
-template <typename AppDataType>
-[[nodiscard]] std::vector<AppDataType> init_appdata(std::pmr::memory_resource *mr,
-                                                    const int num_tasks) {
-  std::vector<AppDataType> all_data;
-  all_data.reserve(num_tasks);
-  for (size_t i = 0; i < num_tasks; ++i) {
-    all_data.emplace_back(mr);  // Each has big vectors
-  }
-  return all_data;
-}
 
-template <typename TaskType>
-void chunk(moodycamel::ConcurrentQueue<TaskType *> &q_cur,
-           moodycamel::ConcurrentQueue<TaskType *> *q_next,
-           std::function<void(TaskType *, cuda::CudaManager &)> func,
-           cuda::CudaManager &mgr) {
-  while (true) {
-    Task *task = nullptr;
-    if (q_cur.try_dequeue(task)) {
-      if (task == nullptr) {
-        // Sentinel => pass it on if there's a next queue and stop
-        if (q_next != nullptr) {
-          q_next->enqueue(nullptr);
-        }
-        break;
-      }
-
-      // -----------------------------------
-      func(task, mgr);
-      // -----------------------------------
-
-      // If there's a next queue, pass the task along
-      if (q_next != nullptr) {
-        q_next->enqueue(task);
-      }
-    } else {
-      std::this_thread::yield();
-    }
-  }
-}
 
 // ---------------------------------------------------------------------
 // Define a Schedule (Nvidia PC)
