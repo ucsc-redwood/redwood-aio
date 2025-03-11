70d7fd74a9cde187dad726c84316f957479edc4a
Yanwen Xu <xuyanwen2012@gmail.com>
2025-03-11 16:50:28 -0700

    save

--- pipe/try-tree-cu/main.cu
+++ pipe/try-tree-cu/main.cu
@@ -44,24 +44,21 @@ void program(const int num_tasks) {
         q_input, &q_12, omp::run_multiple_stages<1, 3, ProcessorType::kBigCore, 8>, mgr);
   });
 
+  std::thread t2(
+      [&]() { chunk<Task, tree::AppData>(q_12, &q_23, cuda::run_multiple_stages<4, 6>, mgr); });
+  std::thread t3([&]() {
+    chunk<Task, tree::AppData>(
+        q_23, nullptr, omp::run_multiple_stages<7, 7, ProcessorType::kLittleCore, 12>, mgr);
+  });
+
   t1.join();
-
-  // std::thread t2([&]() {
-  //   chunk<Task, tree::AppData>(q_12, &q_23, cuda::run_multiple_stages<4, 6>, mgr);
-  // });
-  // std::thread t3([&]() {
-  //   chunk<Task, tree::AppData>(
-  //       q_23, nullptr, omp::run_multiple_stages<7, 7, ProcessorType::kLittleCore, 12>, mgr);
-  // });
-
-  // t1.join();
-  // t2.join();
-  // t3.join();
+  t2.join();
+  t3.join();
 
   // ---------------------------------------------------------------------
   auto end = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-  spdlog::info("Time taken per task: {} microseconds", duration.count() / num_tasks);
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  spdlog::info("Time taken per task: {:.3f} ms", duration.count() / static_cast<double>(num_tasks));
 }
 
 }  // namespace device_pc
@@ -70,42 +67,41 @@ void program(const int num_tasks) {
 // Define a Schedule (Jetson Orin Nano)
 // ---------------------------------------------------------------------
 
-// namespace device_jetson {
-// void program(const int num_tasks) {
-//   cuda::CudaManager mgr;
+namespace device_jetson {
+void program(const int num_tasks) {
+  cuda::CudaManager mgr;
 
-//   auto preallocated_data = init_appdata<cifar_dense::AppData>(&mgr.get_mr(), num_tasks);
+  auto preallocated_data = init_appdata<tree::AppData>(&mgr.get_mr(), num_tasks);
 
-//   // ---------------------------------------------------------------------
-//   moodycamel::ConcurrentQueue<Task *> q_input = init_tasks(preallocated_data, &mgr);
-//   moodycamel::ConcurrentQueue<Task *> q_12;
-//   moodycamel::ConcurrentQueue<Task *> q_23;
+  // ---------------------------------------------------------------------
+  moodycamel::ConcurrentQueue<Task *> q_input = init_tasks(preallocated_data, &mgr);
+  moodycamel::ConcurrentQueue<Task *> q_12;
+  moodycamel::ConcurrentQueue<Task *> q_23;
 
-//   auto start = std::chrono::high_resolution_clock::now();
+  auto start = std::chrono::high_resolution_clock::now();
 
-//   std::thread t1([&]() {
-//     chunk<Task, cifar_dense::AppData>(
-//         q_input, &q_12, omp::run_multiple_stages<1, 3, ProcessorType::kLittleCore, 3>, mgr);
-//   });
-//   std::thread t2([&]() {
-//     chunk<Task, cifar_dense::AppData>(q_12, &q_23, cuda::run_multiple_stages<4, 6>, mgr);
-//   });
-//   std::thread t3([&]() {
-//     chunk<Task, cifar_dense::AppData>(
-//         q_23, nullptr, omp::run_multiple_stages<7, 9, ProcessorType::kLittleCore, 3>, mgr);
-//   });
+  std::thread t1([&]() {
+    chunk<Task, tree::AppData>(
+        q_input, &q_12, omp::run_multiple_stages<1, 3, ProcessorType::kLittleCore, 3>, mgr);
+  });
+  std::thread t2(
+      [&]() { chunk<Task, tree::AppData>(q_12, &q_23, cuda::run_multiple_stages<4, 6>, mgr); });
+  std::thread t3([&]() {
+    chunk<Task, tree::AppData>(
+        q_23, nullptr, omp::run_multiple_stages<7, 7, ProcessorType::kLittleCore, 3>, mgr);
+  });
 
-//   t1.join();
-//   t2.join();
-//   t3.join();
+  t1.join();
+  t2.join();
+  t3.join();
 
-//   // ---------------------------------------------------------------------
-//   auto end = std::chrono::high_resolution_clock::now();
-//   auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-//   spdlog::info("Time taken per task: {} microseconds", duration.count() / num_tasks);
-// }
+  // ---------------------------------------------------------------------
+  auto end = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  spdlog::info("Time taken per task: {:.3f} ms", duration.count() / static_cast<double>(num_tasks));
+}
 
-// }  // namespace device_jetson
+}  // namespace device_jetson
 
 // ---------------------------------------------------------------------
 // Main
@@ -117,7 +113,9 @@ int main(int argc, char **argv) {
   spdlog::set_level(spdlog::level::from_str(g_spdlog_log_level));
 
   if (g_device_id == "pc") {
-    device_pc::program(20);
+    device_pc::program(30);
+  } else if (g_device_id == "jetson") {
+    device_jetson::program(30);
   }
 
   return 0;
